f5d8e305c3f33e6e5c9f484f0475f8c506320ac0
AndreyGOIT <ae.borga@gmail.com>
2022-11-25 20:07:10 +0200

    Task1 step1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import Feedback from './Feedback/Feedback';
+import Statistics from './Statistics/Statistics';
 
 class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
   render() {
     return (
       <>
         <Feedback />
+        <Statistics />
       </>
     );
   }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
 class Statistics extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
   render() {
     return (
       <div className="Statistics">
         <span className="Statistics__title">Statistics</span>
         <div className="Statistics__items">
-          <p className="Statistics__item">Good:</p>
-          <p className="Statistics__item">Neutral:</p>
-          <p className="Statistics__item">Bad:</p>
+          <p className="Statistics__item">Good:{this.state.good}</p>
+          <p className="Statistics__item">Neutral:{this.state.neutral}</p>
+          <p className="Statistics__item">Bad:{this.state.bad}</p>
         </div>
       </div>
     );
