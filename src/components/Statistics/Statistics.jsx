217a283382b643d5f84d42bf906166082207a5dc
AndreyGOIT <ae.borga@gmail.com>
2022-11-30 16:13:50 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Statistics.module.css';
 
+let total = 0;
+let positiveFeedBack = 0;
 class Statistics extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // };
 
   render() {
     return (
@@ -18,6 +20,10 @@ class Statistics extends React.Component {
             Neutral: {this.state.neutral}
           </p>
           <p className={styles.statistics__item}>Bad: {this.state.bad}</p>
+          <p className={styles.statistics__item}>Total: {total}</p>
+          <p className={styles.statistics__item}>
+            Positive feedback: {positiveFeedBack} %
+          </p>
         </div>
       </div>
     );
