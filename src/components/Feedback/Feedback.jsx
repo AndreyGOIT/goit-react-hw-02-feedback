1a5b5347314759cd75f94ee2be7dfb19ada66727
AndreyGOIT <ae.borga@gmail.com>
2022-11-30 16:02:22 +0200

    plus Statistic block

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './Feedback.module.css';
 import Buttons from './Btns';
 
+let total = 0;
+let positiveFeedBack = 0;
 class Feedback extends React.Component {
   static defaultProps = {
     initialValue: 0,
@@ -16,16 +18,22 @@ class Feedback extends React.Component {
     this.setState(prevState => ({
       good: prevState.good + 1,
     }));
+    total += 1;
+    positiveFeedBack = Math.round((this.state.good / total) * 100);
   };
   handleClickNeutral = () => {
     this.setState(prevState => ({
       neutral: prevState.neutral + 1,
     }));
+    total += 1;
+    positiveFeedBack = Math.round((this.state.good / total) * 100);
   };
   handleClickBad = () => {
     this.setState(prevState => ({
       bad: this.state.bad + 1,
     }));
+    total += 1;
+    positiveFeedBack = Math.round((this.state.good / total) * 100);
   };
 
   render() {
@@ -47,6 +55,10 @@ class Feedback extends React.Component {
               Neutral: {this.state.neutral}
             </p>
             <p className={styles.statistics__item}>Bad: {this.state.bad}</p>
+            <p className={styles.statistics__item}>Total: {total}</p>
+            <p className={styles.statistics__item}>
+              Positive feedback: {positiveFeedBack} %
+            </p>
           </div>
         </div>
       </>
