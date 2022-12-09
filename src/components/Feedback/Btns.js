63e5ca98bae75bc1e6a44fa2c8d9919c2dc302d0
AndreyGOIT <ae.borga@gmail.com>
2022-12-09 14:02:54 +0200

    backup

--- src/components/Feedback/Btns.js
+++ src/components/Feedback/Btns.js
@@ -1,20 +1,25 @@
-// import React from 'react';
-// import styles from './Btns.module.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Btns.module.css';
 
-// const Buttons = ({ onLeaveFeedback }) => {
-//   return (
-//     <div className={styles.feedback__btns}>
-//       <button type="button" onClick={onLeaveFeedback}>
-//         Good
-//       </button>
-//       <button type="button" onClick={onLeaveFeedback}>
-//         Neutral
-//       </button>
-//       <button type="button" onClick={onLeaveFeedback}>
-//         Bad
-//       </button>
-//     </div>
-//   );
-// };
+const Buttons = ({ onLeaveFeedback }) => {
+  return (
+    <div className={styles.feedback__btns}>
+      <button type="button" name="good" onClick={onLeaveFeedback}>
+        Good
+      </button>
+      <button type="button" name="neutral" onClick={onLeaveFeedback}>
+        Neutral
+      </button>
+      <button type="button" name="bad" onClick={onLeaveFeedback}>
+        Bad
+      </button>
+    </div>
+  );
+};
 
-// export default Buttons;
+export default Buttons;
+
+Buttons.propTypes = {
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,18 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { Button } from './FeedbackOptions.styled';
 
-export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+export const FeedbackOptions = ({ options }) => {
   return (
     <div>
       {options.map(key => (
-        <Button key={key} name={key} type="button" onClick={onLeaveFeedback}>
+        <Button
+          key={key}
+          name={key}
+          type="button"
+          onClick={() => {
+            console.log('c');
+          }}
+        >
           {key}
         </Button>
       ))}
@@ -13,7 +20,7 @@ export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   );
 };
 
-FeedbackOptions.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.string.isRequired),
-  onLeaveFeedback: PropTypes.func.isRequired,
-};
+// FeedbackOptions.propTypes = {
+//   options: PropTypes.arrayOf(PropTypes.string.isRequired),
+//   onLeaveFeedback: PropTypes.func.isRequired,
+// };
